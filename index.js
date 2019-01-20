04dddcb564c422ef93ad9eaf838261ac555a02fa
kyle.rb <kyleryanboyle@gmail.com>
2019-01-20 01:10:33 -0500

    Rewrote some stuff to get rid of redundancy, added additional reminders after assert intents, and vectorized the logo

--- index.js
+++ index.js
@@ -1,15 +1,50 @@
 var Alexa = require('alexa-sdk');
 var APP_ID = "amzn1.ask.skill.<skill id here>";
 var TABLE_NAME = "DOG_DATA_ATTRIBUTES";
-var DATA_POINTS = {
-  FEED: { attribute: 'fedTime',      awaiting: 'shouldFeed' },
-  MED:  { attribute: 'medGivenTime', awaiting: 'shouldGiveMed' },
-  OUT:  { attribute: 'letOutTime',   awaiting: 'shouldLetOut' },
-  WALK: { attribute: 'walkedTime',   awaiting: 'shouldWalk' }
+
+var ACTIONS = { // four different actions with different data
+  FEED: {
+    TIME: 'fedTime', // name of attribute for storing time in the database
+    AWAITING: 'shouldFeed', // value for 'awaiting' attribute in the database
+    STRING: { // for inserting into response strings, with different tenses
+      PRESENT: 'feed',
+      PAST: 'fed',
+      ING: 'feeding',
+    },
+  },
+  MED: {
+    TIME: 'medGivenTime',
+    AWAITING: 'shouldGiveMed',
+    STRING: {
+      PRESENT: 'give medication to',
+      PAST: 'gave medication to',
+      ING: 'giving medication to',
+    },
+  },
+  OUT: {
+    TIME: 'letOutTime',
+    AWAITING: 'shouldLetOut',
+    STRING: {
+      PRESENT: 'let out',
+      PAST: 'let out',
+      ING: 'letting out',
+    },
+  },
+  WALK: {
+    TIME: 'walkedTime',
+    AWAITING: 'walkedTime',
+    STRING: {
+      PRESENT: 'walk',
+      PAST: 'walked',
+      ING: 'walking',
+    },
+  },
 };
 
+
+
 var welcomePrompt = "I can remember when you last fed, walked, let out, or gave your dog medication.";
-var welcomeReprompt = "Ask me about one of those things, or tell me that you've done one of them.";
+var welcomeReprompt = "Ask me about one of those things, or tell me when you've done one of them.";
 
 exports.handler = function(event, context, callback) {
   var alexa = Alexa.handler(event, context, callback);
@@ -20,157 +55,73 @@ exports.handler = function(event, context, callback) {
 };
 
 var handlers = {
+  // General startup and shutdown intents.
   'LaunchRequest': function() {
     this.emit(':ask', welcomePrompt, welcomeReprompt);
   },
   'SessionEndedRequest': function () {
-    this.attributes['awaiting'] = undefined;
+    this.attributes['awaiting'] = null;
     this.emit(':saveState', true);
   },
 
 
-  'IFed': function() {
-    var dogName = "the dog"; //this.event.request.intent.slots.dogName.value || "the dog";
-    this.attributes[DATA_POINTS.FEED.attribute] = Date.now();
-    this.emit(':tell', "OK, I'll remember that you fed " + dogName + ".");
-  },
-  'IGaveMedication': function() {
-    var dogName = "the dog"; //this.event.request.intent.slots.dogName.value || "the dog";
-    this.attributes[DATA_POINTS.MED.attribute] = Date.now();
-    this.emit(':tell', "OK, I'll remember that you gave medication to " + dogName + ".");
-  },
-  'ILetOut': function() {
-    var dogName = "the dog"; //this.event.request.intent.slots.dogName.value || "the dog";
-    this.attributes[DATA_POINTS.OUT.attribute] = Date.now();
-    this.emit(':tell', "OK, I'll remember that you let out " + dogName + ".");
-  },
-  'IWalked': function() {
-    var dogName = "the dog"; //this.event.request.intent.slots.dogName.value || "the dog";
-    this.attributes[DATA_POINTS.WALK.attribute] = Date.now();
-    this.emit(':tell', "OK, I'll remember that you walked " + dogName + ".");
-  },
+  // These are Assert Intents, asserting that some action has been done.
+  // If one of these is called, call the general assert handler with the appropriate action.
+  'IFed':            function() { handleAssertIntent(ACTIONS.FEED, this); },
+  'IGaveMedication': function() { handleAssertIntent(ACTIONS.MED, this);  },
+  'ILetOut':         function() { handleAssertIntent(ACTIONS.OUT, this);  },
+  'IWalked':         function() { handleAssertIntent(ACTIONS.WALK, this); },
+
+  // These are Ask Intents, asking whether some action has been done.
+  // If one of these is called, call the general ask handler with the appropriate action.
+  'DidIFeed':           function() { handleAskIntent(ACTIONS.FEED, this); },
+  'DidIGiveMedication': function() { handleAskIntent(ACTIONS.MED, this);  },
+  'DidILetOut':         function() { handleAskIntent(ACTIONS.OUT, this);  },
+  'DidIWalk':           function() { handleAskIntent(ACTIONS.WALK, this); },
 
 
-  'DidIFeed': function() {
-    var dogName = "the dog"; //this.event.request.intent.slots.dogName.value || "the dog";
-    var timeFed = this.attributes[DATA_POINTS.FEED.attribute];
-    if (timeFed === undefined) {
-      this.attributes['awaiting'] = DATA_POINTS.FEED.awaiting;
-      this.emit(':ask', "You haven't told me about feeding " + dogName + " yet. Are you going to feed " + dogName + " now?");
-    }
-    else {
-      var hoursDifference = (((Date.now() - timeFed) / 1000) / 60) / 60;
-      var timePassedString = getTimePassedString(hoursDifference);
-      if (hoursDifference >= 4) {
-        this.attributes['awaiting'] = DATA_POINTS.FEED.awaiting;
-        this.emit(':ask', "You last fed " + dogName + timePassedString + ". Are you going to feed " + dogName + " now?");
-      }
-      else {
-        this.emit(':tell', "You last fed " + dogName + timePassedString + ".");
-      }
-    }
-  },
-  'DidIGiveMedication': function() {
-    var dogName = "the dog"; //this.event.request.intent.slots.dogName.value || "the dog";
-    var timeFed = this.attributes[DATA_POINTS.MED.attribute];
-    if (timeFed === undefined) {
-      this.attributes['awaiting'] = DATA_POINTS.MED.awaiting;
-      this.emit(':ask', "You haven't told me about giving medication to " + dogName + " yet. Are you going to give medication to " + dogName + " now?");
-    }
-    else {
-      var hoursDifference = (((Date.now() - timeFed) / 1000) / 60) / 60;
-      var timePassedString = getTimePassedString(hoursDifference);
-      if (hoursDifference >= 4) {
-        this.attributes['awaiting'] = DATA_POINTS.MED.awaiting;
-        this.emit(':ask', "You last gave medication to " + dogName + timePassedString + ". Are you going to give medication to " + dogName + " now?");
-      }
-      else {
-        this.emit(':tell', "You last gave medication to " + dogName + timePassedString + ".");
-      }
-    }
-  },
-  'DidILetOut': function() {
-    var dogName = "the dog"; //this.event.request.intent.slots.dogName.value || "the dog";
-    var timeFed = this.attributes[DATA_POINTS.OUT.attribute];
-    if (timeFed === undefined) {
-      this.attributes['awaiting'] = DATA_POINTS.OUT.awaiting;
-      this.emit(':ask', "You haven't told me about letting out " + dogName + " yet. Are you going to let out " + dogName + " now?");
-    }
-    else {
-      var hoursDifference = (((Date.now() - timeFed) / 1000) / 60) / 60;
-      var timePassedString = getTimePassedString(hoursDifference);
-      if (hoursDifference >= 4) {
-        this.attributes['awaiting'] = DATA_POINTS.OUT.awaiting;
-        this.emit(':ask', "You last let out " + dogName + timePassedString + ". Are you going to let out " + dogName + " now?");
-      }
-      else {
-        this.emit(':tell', "You last let out " + dogName + timePassedString + ".");
-      }
-    }
-  },
-  'DidIWalk': function() {
-    var dogName = "the dog"; //this.event.request.intent.slots.dogName.value || "the dog";
-    var timeFed = this.attributes[DATA_POINTS.WALK.attribute];
-    if (timeFed === undefined) {
-      this.attributes['awaiting'] = DATA_POINTS.WALK.awaiting;
-      this.emit(':ask', "You haven't told me about walking " + dogName + " yet. Are you going to walk " + dogName + " now?");
-    }
-    else {
-      var hoursDifference = (((Date.now() - timeFed) / 1000) / 60) / 60;
-      var timePassedString = getTimePassedString(hoursDifference);
-      if (hoursDifference >= 4) {
-        this.attributes['awaiting'] = DATA_POINTS.WALK.awaiting;
-        this.emit(':ask', "You last walked " + dogName + timePassedString + ". Are you going to walk " + dogName + " now?");
-      }
-      else {
-        this.emit(':tell', "You last walked " + dogName + timePassedString + ".");
-      }
-    }
-  },
-
-
+  // If the user says "Yes", figure out what they were responding to, and mark that action as having
+  //   been done at the current time.
   'AMAZON.YesIntent': function() {
-    var response = "";
+    var action, response;
     switch (this.attributes['awaiting']) {
-      case DATA_POINTS.FEED.awaiting:
-        this.attributes[DATA_POINTS.FEED.attribute] = Date.now();
-        response = "OK, I'll remember that you fed the dog.";
-        break;
-      case DATA_POINTS.MED.awaiting:
-        this.attributes[DATA_POINTS.MED.attribute] = Date.now();
-        response = "OK, I'll remember that you gave medication to the dog.";
-        break;
-      case DATA_POINTS.OUT.awaiting:
-        this.attributes[DATA_POINTS.OUT.attribute] = Date.now();
-        response = "OK, I'll remember that you let out the dog.";
-        break;
-      case DATA_POINTS.WALK.awaiting:
-        this.attributes[DATA_POINTS.WALK.attribute] = Date.now();
-        response = "OK, I'll remember that you walked the dog.";
-        break;
-      default:
-        response = "Sorry, I'm not sure what you meant by that.";
+      case ACTIONS.FEED.AWAITING: action = ACTIONS.FEED; break;
+      case ACTIONS.MED.AWAITING: action = ACTIONS.MED; break;
+      case ACTIONS.OUT.AWAITING: action = ACTIONS.OUT; break;
+      case ACTIONS.WALK.AWAITING: action = ACTIONS.WALK; break;
+      default: action = null;
     }
-    this.attributes['awaiting'] = undefined;
+    if (action) {
+      this.attributes[action.TIME] = Date.now();
+      response = "OK, I'll remember that you " + action.STRING.PAST + " the dog.";
+    }
+    else {
+      response = "Sorry, I'm not sure what you meant by that.";
+    }
+    this.attributes['awaiting'] = null;
     this.emit(':tell', response);
   },
+  // If the user says "No", as long as they were responding to something, just respond "OK".
   'AMAZON.NoIntent': function() {
     var response = "";
     switch (this.attributes['awaiting']) {
-      case DATA_POINTS.FEED.awaiting:
-      case DATA_POINTS.MED.awaiting:
-      case DATA_POINTS.OUT.awaiting:
-      case DATA_POINTS.WALK.awaiting:
+      case ACTIONS.FEED.AWAITING:
+      case ACTIONS.MED.AWAITING:
+      case ACTIONS.OUT.AWAITING:
+      case ACTIONS.WALK.AWAITING:
         response = "OK";
         break;
       default:
         response = "Sorry, I'm not sure what you meant by that.";
     }
-    this.attributes['awaiting'] = undefined;
+    this.attributes['awaiting'] = null;
     this.emit(':tell', response);
   },
+
+
+  // Various other standard intents.
   'AMAZON.HelpIntent': function() {
-    this.attributes['awaiting'] = undefined;
+    this.attributes['awaiting'] = null;
     this.emit(':ask', welcomePrompt, welcomeReprompt);
   },
   'AMAZON.CancelIntent': function() {
@@ -184,19 +135,53 @@ var handlers = {
   }
 };
 
+
+
+function handleAssertIntent(action, alexaContext) { // handles intents like "I fed the dog"
+  var timeLastDone = alexaContext.attributes[action.TIME];
+  var timeReminder = "";
+  if (timeLastDone !== undefined) { // add extension with time 
+    var hoursDifference = (((Date.now() - timeLastDone) / 1000) / 60) / 60;
+    timeReminder = " The last time you did so was " + getTimePassedString(hoursDifference) + ".";
+  }
+  alexaContext.attributes[action.TIME] = Date.now();
+  alexaContext.emit(':tell', "OK, I'll remember that you " + action.STRING.PAST + " the dog." + timeReminder);
+}
+
+function handleAskIntent(action, alexaContext) { // handles intents like "did I feed the dog?"
+  var timeLastDone = alexaContext.attributes[action.TIME];
+  if (timeLastDone === undefined) {
+    alexaContext.attributes['awaiting'] = action.AWAITING;
+    alexaContext.emit(':ask', "You haven't told me about " + action.STRING.ING + " the dog yet. Are you going to " + action.STRING.PRESENT + " the dog now?");
+  }
+  else {
+    var hoursDifference = (((Date.now() - timeLastDone) / 1000) / 60) / 60;
+    var timePassedString = getTimePassedString(hoursDifference);
+    if (hoursDifference >= 4) {
+      alexaContext.attributes['awaiting'] = AWAITING.FEED;
+      alexaContext.emit(':ask', "You last " + action.STRING.PAST + " the dog " + timePassedString + ". Are you going to " + action.STRING.PRESENT + " the dog now?");
+    }
+    else {
+      alexaContext.emit(':tell', "You last " + action.STRING.PAST + " the dog " + timePassedString + ".");
+    }
+  }
+}
+
+
+// Helper function for constructing a string with 'hours ago' or 'days ago', etc.
 function getTimePassedString(hoursDifference) {
   var timePassedString;
   if (hoursDifference >= 45) {
-    " about " + Math.round(hoursDifference / 24) + " days ago";
+    timePassedString = "about " + Math.round(hoursDifference / 24) + " days ago";
   }
   else if (hoursDifference < 1) {
-    timePassedString = " less than an hour ago";
+    timePassedString = "less than an hour ago";
   }
   else if (hoursDifference < 1.5) {
-    timePassedString = " about an hour ago"; // singular
+    timePassedString = "about an hour ago"; // singular
   }
-  else {
-    timePassedString = " about " + Math.round(hoursDifference) + " hours ago"; // plural
+  else { // between 2 and 44
+    timePassedString = "about " + Math.round(hoursDifference) + " hours ago"; // plural
   }
   return timePassedString;
 }
